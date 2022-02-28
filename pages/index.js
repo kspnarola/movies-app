decd5d6608ec0538d423a629d6f591c77effe0e3
Kishan Patel <ksp@narola.email>
2022-02-28 15:42:40 +0530

    changes fix

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,8 @@ import { useEffect, useState } from 'react'
 import { Container } from 'react-bootstrap'
 import MoviesList from '../components/MoviesList'
 import { MOVIES_LIST_DATA } from '../mockData/mockData'
-import MoviesData from '../mockData/data.json'
 import FilterBox from '../components/FilterBox'
+import Head from 'next/head'
 
 export default function Home() {
   const [searchMovie, setSearchMovie] = useState("")
@@ -13,14 +13,22 @@ export default function Home() {
     if (searchMovie) {
       const filteredMovies = MOVIES_LIST_DATA.filter(movie => movie.title.toLowerCase().includes(searchMovie.toLowerCase()));
       setMovies(filteredMovies);
+    } else {
+      setMovies(MOVIES_LIST_DATA);
     }
   }, [searchMovie])
 
 
   return (
-    <Container className="home-page">
-      <FilterBox searchMovie={searchMovie} setSearchMovie={setSearchMovie} />
-      <MoviesList movies={movies} />
-    </Container>
+    <>
+      <Head>
+        <title>Movie App</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Container className="home-page">
+        <FilterBox searchMovie={searchMovie} setSearchMovie={setSearchMovie} />
+        <MoviesList movies={movies} />
+      </Container>
+    </>
   )
 }
