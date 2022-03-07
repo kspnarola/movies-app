c22e0a9d1315c47189d3c4421e75842504aef5c1
Kishan Patel <ksp@narola.email>
2022-03-07 11:55:35 +0530

    changes fix

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   var myHeaders = new Headers();
   myHeaders.append("Authorization", "Basic a3NwQG5hcm9sYS5lbWFpbDpCNmo0THFGM21VRG1VOVF2VFh0aTg2QjI=");
   myHeaders.append("Cookie", "atlassian.xsrf.token=c107d769-5ae7-415b-9ffe-62657f26d5a4_f2715c16f3d17b389a6dd17a4933066d6454e12d_lin");
